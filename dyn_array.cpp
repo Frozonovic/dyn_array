f7e5ec0eea7183328b4274332bf95cc5fa24f1a7
Brian Snider <bsnider@georgefox.edu>
2022-01-04 09:59:45 -0800

    initial commit

---
+++ dyn_array.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file dyn_array.cpp
+ * @brief A dynamic array demonstration program.
+ *
+ * A program that demonstrates dynamic allocation of arrays
+ * on the heap.
+ *
+ * @author Brian R. Snider (bsnider@georgefox.edu)
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    // TODO determine array capacity from command-line args
+
+    // TODO dynamically allocate array
+
+    // TODO populate array
+
+    // TODO iterate through array and print contents to stdout
+
+    // TODO free allocated array
+}
+
