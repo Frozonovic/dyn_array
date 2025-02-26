a3ea8ea2612900888e1b391e7e42cd58fb3015ad
James Lee <93396980+Frozonovic@users.noreply.github.com>
2025-02-26 12:04:33 -0800

    Update dyn_array.cpp

--- dyn_array.cpp
+++ dyn_array.cpp
@@ -8,34 +8,38 @@
  * @author Brody Lee (blee20@georgefox.edu)
  */
 
-#include <iostream>
+#include <cstdlib>
 #include <cmath>
-
+#include <iostream>
+#include <stdexcept>
 
 int main(int argc, char* argv[])
 {
-    // Check arg count
-    if (argc > 2) {
-        // TODO: Throw error?
-    }
-
-    // Determine array capacity from command-line args
-    auto capacity = static_cast<unsigned long long>(*argv[1]);
-
-    // Dynamically allocate array
-    auto arr = new unsigned long long[capacity];
-
-    // Populate array
-    for (int i = 0; i < capacity; i++) {
-        arr[i] = pow(2, i);
-    }
-
-    // Iterate through array and print contents to stdout
-    for(int i = 0; i < capacity; i++) {
-        std::cout << arr[i] << std::endl;
-    }
-
-    // Free allocated array
-    delete[] arr;
+    try {    
+        // Check arg count
+        if (argc != 2) {
+            throw std::invalid_argument("Error: Invalid argument count");
+        }
+    
+        // Determine array capacity from command-line args
+        auto capacity = static_cast<unsigned long long>(*argv[1]);
+    
+        // Dynamically allocate array
+        auto arr = new unsigned long long[capacity];
+    
+        // Populate array
+        for (int i = 0; i < capacity; i++) {
+            arr[i] = pow(2, i);
+        }
+    
+        // Iterate through array and print contents to stdout
+        for(int i = 0; i < capacity; i++) {
+            std::cout << arr[i] << std::endl;
+        }
+    
+        // Free allocated array
+        delete[] arr;
+    } catch (const std::invalid_argument& e) {
+        std::cerr << e.what() << std::endl;
 }
 
